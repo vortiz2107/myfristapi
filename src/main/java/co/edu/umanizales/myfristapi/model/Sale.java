64768370d7ff7948e9d388aadfaa3b0603ef7004
vortiz2107 <vortiz113799@umanizales.edu.co>
2025-03-24 22:32:26 -0500

    Agregar sale y store

---
+++ src/main/java/co/edu/umanizales/myfristapi/model/Sale.java
@@ -0,0 +1,17 @@
+package co.edu.umanizales.myfristapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Sale {
+    private String identification;
+    private String product;
+    private String date;
+
+
+}
---
+++ src/main/java/co/edu/umanizales/myfristapi/model/Store.java
@@ -0,0 +1,15 @@
+package co.edu.umanizales.myfristapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Store {
+    private String code;
+    private String name;
+    private String address;
+    private Location city;
+}
